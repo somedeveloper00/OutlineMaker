b5fe3c8b43d4d241b8f1f7a45d99c8e146e1b85c
Saeed Barari <brarari.saeed.am@outlook.com>
2022-10-05 22:37:49 +0330

    shader done

---
+++ Assets/Scripts/PathOutlinerImage/PathOutlinerImage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+[ExecuteAlways]
+public class PathOutlinerImage : MonoBehaviour
+{
+	public Material material;
+	public Renderer renderer;
+
+	public Color color;
+	public int length;
+	public Circle[] circles;
+	
+	[Serializable]
+	public class Circle
+	{
+		public Vector2 center;
+		public float radius;
+	}
+
+	private void OnValidate()
+	{
+		if (renderer == null) renderer = GetComponent<Renderer>();
+		if (renderer.sharedMaterial != material)
+			renderer.sharedMaterial = material;
+	}
+
+	private void OnRenderObject()
+	{
+		UpdateShader();
+	}
+
+	private void UpdateShader()
+	{
+		if(renderer == null || material == null) return;
+
+		var points = circles.Select(c => (Vector4)c.center).ToArray();
+		var radiuses = circles.Select(c => c.radius).ToArray();
+		
+		var prop = new MaterialPropertyBlock();
+		prop.SetColor("_Color", color);
+		prop.SetFloat("_Count", circles.Length);
+		prop.SetFloat("_Length", length);
+		prop.SetVectorArray("_Centers", points);
+		prop.SetFloatArray("_Radiuses", radiuses);
+		renderer.SetPropertyBlock(prop);
+	}
+}
